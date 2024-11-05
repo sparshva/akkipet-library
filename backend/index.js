29e81238bfcbb455ef6f237d1a4cf053dae1b512
sparshva <ppshah1202@gmail.com>
2024-11-05 22:23:20 +0530

    added cors

--- backend/index.js
+++ backend/index.js
@@ -10,13 +10,20 @@ import userRouter from "./routes/user.js";
 import orderRouter from "./routes/order.js";
 
 app.use(express.json());
+app.options("*", cors());
 app.use(
   cors({
     credentials: true,
     origin: true,
     optionsSuccessStatus: 200,
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   })
 );
+
+app.use((req, res, next) => {
+  console.log(`${req.method} request for ${req.url}`);
+  next();
+});
 
 const connectDb = async () => {
   try {
